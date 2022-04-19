491dbbd55bc1aaf00ef66646cd4677b2bb20b89f
Jiefu Ling <jeffling0603@gmail.com>
2022-04-19 01:54:35 -0700

    HW1 almost completed
    String getRotatedFullName(int shift) is not completed.

---
+++ src/main/java/JiefuLing.java
@@ -0,0 +1,27 @@
+public class JiefuLing {
+
+	public String getFullName() {
+		return "Jiefu Ling";
+	}
+	
+	public String getFirstName() {
+		return "Jiefu";
+	}
+	
+	public String getLastName() {
+		return "Ling";
+	}
+	
+	public String getUCInetID() {
+		return "jieful2";
+	}
+	
+	public int getStudentNumber() {
+		return 25008346;
+	}
+	
+	public String getRotatedFullName(int shift) {
+		return "shit";
+	}
+
+}
